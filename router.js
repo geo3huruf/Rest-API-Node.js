102adf821aadb871e77bd2c41db9724a3555cf02
geo3huruf <67548372+geo3huruf@users.noreply.github.com>
2021-07-02 22:35:57 +0700

    Update router.js

--- router.js
+++ router.js
@@ -31,7 +31,7 @@ const respon = {
 };
 
 router.get("/", async (req, res) => {
-  res.redirect("https://www.apimau.ga");
+  res.redirect("https://www.google.com");
 });
 
 router.get("/igs", async (req, res) => {
